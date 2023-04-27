f015654aac1d2a804ccb5a5b92e250eb2a61cc19
adeskmath <adeskmath@gmail.com>
2023-04-27 19:27:29 +0300

    CustomerController: added searchNew() - alternative - without using utils class JsonOperator

--- src/main/java/com/adeskmath/backend/shop/controller/CustomerController.java
+++ src/main/java/com/adeskmath/backend/shop/controller/CustomerController.java
@@ -7,6 +7,7 @@ import com.adeskmath.backend.shop.search.SearchLowestRank;
 import com.adeskmath.backend.shop.search.SearchProductMin;
 import com.adeskmath.backend.shop.service.CustomerService;
 import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -27,6 +29,51 @@ public class CustomerController {
     private final CustomerService customerService;
     public CustomerController(CustomerService customerService) {
         this.customerService = customerService;
+    }
+
+    @PostMapping("/searchNew")
+    public ResponseEntity<Map<String, Object>> searchNew(@RequestBody JsonNode searchCriterias) throws IOException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        JsonNode rootNode = objectMapper.readTree(searchCriterias.traverse());
+
+        List<Map<String, Object>> mapList = new ArrayList<>();
+
+        rootNode.path("criterias").forEach(jsonNodeCriteria -> {
+            Map<String, Object> map = new HashMap<>();
+            List<Customer> list = getCustomers(objectMapper, jsonNodeCriteria);
+            map.put("criteria", jsonNodeCriteria);
+            map.put("results", list);
+            mapList.add(map);
+        });
+
+        Map<String, Object> map = new HashMap<>();
+        map.put("type", "search");
+        map.put("results", mapList);
+
+        return ResponseEntity.ok(map);
+    }
+
+    private List<Customer> getCustomers(ObjectMapper objectMapper, JsonNode jsonNode) {
+        List<Customer> list = new ArrayList<>();
+        switch (jsonNode.fieldNames().next()) {
+            case "lastName" -> {
+                SearchLastName name = objectMapper.convertValue(jsonNode, SearchLastName.class);
+                list = customerService.findByLastName(name.getLastName());
+            }
+            case "lowestRank" -> {
+                SearchLowestRank lowest = objectMapper.convertValue(jsonNode, SearchLowestRank.class);
+                list = customerService.findByLowestRank(lowest.getLowestRank());
+            }
+            case "minExpenses" -> {
+                SearchExpenseRange expense = objectMapper.convertValue(jsonNode, SearchExpenseRange.class);
+                list = customerService.findByExpenseRange(expense.getMinExpenses(), expense.getMaxExpenses());
+            }
+            case "productName" -> {
+                SearchProductMin product = objectMapper.convertValue(jsonNode, SearchProductMin.class);
+                list = customerService.findByProductMinTimes(product.getProductName(), product.getMinTimes());
+            }
+        }
+        return list;
     }
 
     @PostMapping("/search")
